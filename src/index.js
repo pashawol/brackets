2fab9183f9221235d3c6a95dbffcb80ebeed52eb
Pavel Khalunin <wol1414@gmail.com>
2019-03-24 02:29:33 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = []; 
+
+  for(let i = 0; i < str.length; ++i){
+    let c = str[i]; 
+      if(bracketsConfig[c]){
+        
+        return true;
+      } else {
+        return false;
+        
+      } 
+    
+    } 
+  }
+
+
